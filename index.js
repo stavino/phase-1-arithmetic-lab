a09df26137dd8c0baa0d923c54b373976b0aa60c
Stephen Avino <stephen.avino@gmail.com>
2021-11-02 09:40:41 -0400

    commit tree f1512f7b814d02529c3dd85b3afc3411131e93f3

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 10;
+let num2 = 6.2;
+let num3 = 32;
+let num4 = 14;
+let multiply = (num1 * num2);
+let random = Math.floor(Math.random() * 10) + 1;
+let mod = (num3 %= num4);
+let max = Math.max(5,10,15,20);
